13ec0c858fe1b5f9f691304c1f05b0b2418b5504
cottarius <cotarius@yandex.ru>
2024-04-05 15:45:18 +0300

    1

--- src/main/java/ru/cotarius/springDMDEV/ApplicationRunner.java
+++ src/main/java/ru/cotarius/springDMDEV/ApplicationRunner.java
@@ -2,6 +2,7 @@ package ru.cotarius.springDMDEV;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.cotarius.springDMDEV.database.repository.CompanyRepository;
 import ru.cotarius.springDMDEV.database.repository.UserRepository;
 import ru.cotarius.springDMDEV.ioc.Container;
@@ -11,19 +12,9 @@ import ru.cotarius.springDMDEV.service.UserService;
 public class ApplicationRunner {
 
 	public static void main(String[] args) {
-		Container container = new Container();
-
-//		ConnectionPool connectionPool = new ConnectionPool();
-//		UserRepository userRepository = new UserRepository(connectionPool);
-//		CompanyRepository companyRepository = new CompanyRepository(connectionPool);
-//		UserService userService = new UserService(userRepository, companyRepository);
-
-//		ConnectionPool connectionPool = container.get(ConnectionPool.class);
-//		UserRepository userRepository = container.get(UserRepository.class);
-//		CompanyRepository companyRepository = container.get(CompanyRepository.class);
-
-		UserService userService = container.get(UserService.class);
-		//TODO:
+		var context = new ClassPathXmlApplicationContext("application.xml");
+		var connectionPool = context.getBean("pool1", ConnectionPool.class);
+		System.out.println(connectionPool);
 	}
 
 }
