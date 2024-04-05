c0369a35bb86e375083d633bf637918daeb4905e
cottarius <cotarius@yandex.ru>
2024-04-05 15:20:49 +0300

    2

--- src/main/java/ru/cotarius/springDMDEV/ApplicationRunner.java
+++ src/main/java/ru/cotarius/springDMDEV/ApplicationRunner.java
@@ -4,16 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ru.cotarius.springDMDEV.database.repository.CompanyRepository;
 import ru.cotarius.springDMDEV.database.repository.UserRepository;
+import ru.cotarius.springDMDEV.ioc.Container;
 import ru.cotarius.springDMDEV.pool.ConnectionPool;
 import ru.cotarius.springDMDEV.service.UserService;
 
 public class ApplicationRunner {
 
 	public static void main(String[] args) {
-		ConnectionPool connectionPool = new ConnectionPool();
-		UserRepository userRepository = new UserRepository(connectionPool);
-		CompanyRepository companyRepository = new CompanyRepository(connectionPool);
-		UserService userService = new UserService(userRepository, companyRepository);
+		Container container = new Container();
+
+//		ConnectionPool connectionPool = new ConnectionPool();
+//		UserRepository userRepository = new UserRepository(connectionPool);
+//		CompanyRepository companyRepository = new CompanyRepository(connectionPool);
+//		UserService userService = new UserService(userRepository, companyRepository);
+
+//		ConnectionPool connectionPool = container.get(ConnectionPool.class);
+//		UserRepository userRepository = container.get(UserRepository.class);
+//		CompanyRepository companyRepository = container.get(CompanyRepository.class);
+
+		UserService userService = container.get(UserService.class);
 		//TODO:
 	}
 
---
+++ src/main/java/ru/cotarius/springDMDEV/ioc/Container.java
@@ -0,0 +1,7 @@
+package ru.cotarius.springDMDEV.ioc;
+
+public class Container {
+    public <T> T get(Class<T> clazz){
+        return null;
+    }
+}
